80781169f1f4617339b7160a4a0b78f5453f9a25
asket21 <kronarkrouse@gmail.com>
2024-04-18 10:13:23 +0300

    Вычищаем проект от другоого проекта. Попытка 1, + пробуем gitignore

--- index.js
+++
@@ -1,7 +0,0 @@
-require("events").EventEmitter.defaultMaxListeners = 0;
-const dictionary = require("./AppModules/dictionary");
-const launcher = require("./AppModules/launcher");
-
-console.log(dictionary.global.hello);
-
-launcher.run();
